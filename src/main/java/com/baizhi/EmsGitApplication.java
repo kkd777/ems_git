f5e0e30fabc0e2bc9967ee1128a4bd8913feb1e5
qq <1186128716@qq.com>
2019-04-23 11:18:35 +0800

    修改

--- src/main/java/com/baizhi/EmsGitApplication.java
+++ src/main/java/com/baizhi/EmsGitApplication.java
@@ -3,10 +3,13 @@ package com.baizhi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class EmsGitApplication {
 
     public static void main(String[] args) {
+        System.out.println(123);
         SpringApplication.run(EmsGitApplication.class, args);
     }
 
